2ae8289ce5a3284f485cf18afaad1117a1dbf4c3
Andriy@DESKTOP-I890ONF.(none) <f.andriy9370@gmail.com>
2020-05-16 20:06:02 +0300

    Додано скрипт завантаження карти на скрол

--- js/main.js
+++ js/main.js
@@ -28,4 +28,14 @@ $(window).load(function() {
     return false
   })
   $('input[type="tel"]').mask("+7 (999) 999-99-99"); 
+
+  var reviews = $('.reviews');
+  var reviewsTop = reviews.offset().top;
+  $(window).bind('scroll', function(){
+    var windowTop = $(this).scrollTop();
+    if (windowTop > reviewsTop){
+      $('#map').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5075.481457965812!2d30.494796556812553!3d50.50178302396007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d20f398425ff%3A0xdc489e974e98399d!2z0J7QsdC-0LvQvtC90Yw!5e0!3m2!1suk!2sua!4v1588589269827!5m2!1suk!2sua" width="100%" height="410" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>')
+      $(window).unbind('scroll');
+    }  
+  }) 
 });
